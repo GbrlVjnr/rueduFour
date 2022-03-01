1866ed2b790d678d93167e30ca89f29082debd43
Gabriel Vejnar <gabrielvejnar@macminidegabriel.home>
2022-03-01 12:46:05 +0100

    First commit

--- rueduFour/settings.py
+++ rueduFour/settings.py
@@ -156,4 +156,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
+import django_heroku
+django_heroku.settings(locals())
