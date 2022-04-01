60110fa4d3ba1f298aa82e334d9be5579a0117c0
Gabriel Vejnar <gabrielvejnar@macminidegabriel.home>
2022-04-01 12:47:09 +0200

    corrected config file

--- comptabilite/admin.py
+++ comptabilite/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Account, Entry, Distribution
+from .models import Account, Entry, Distribution, PrintsDistribution
 
 admin.site.register(Account)
 admin.site.register(Entry)
 admin.site.register(Distribution)
+admin.site.register(PrintsDistribution)
+
