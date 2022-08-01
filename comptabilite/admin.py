26e12da7f93ace6e0a2e8534aa211b25e1702b27
Gabriel Vejnar <gabrielvejnar@MacBook-Pro-de-Gabriel.local>
2022-08-01 19:17:30 +0200

    inline Distribution admin

--- comptabilite/admin.py
+++ comptabilite/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-
 from .models import Account, Entry, Distribution, PrintsDistribution
 
+class DistributionInlineAdmin(admin.TabularInline):
+    model = Distribution
+
+class EntryAdmin(admin.ModelAdmin):
+    inlines = [DistributionInlineAdmin]
+
+
 admin.site.register(Account)
-admin.site.register(Entry)
+admin.site.register(Entry, EntryAdmin)
 admin.site.register(Distribution)
 admin.site.register(PrintsDistribution)
 
